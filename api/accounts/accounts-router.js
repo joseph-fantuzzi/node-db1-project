17a2a947fbcc50575a4ff57050376ffb0e8565fa
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-04-18 17:23:57 -0400

    update model function in place and most requests complete

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -29,8 +29,13 @@ router.post("/", async (req, res, next) => {
   }
 });
 
-router.put("/:id", (req, res, next) => {
-  // DO YOUR MAGIC
+router.put("/:id", async (req, res, next) => {
+  try {
+    const updatedAccount = await Accounts.updateById(req.params.id, req.body);
+    res.status(200).json(updatedAccount);
+  } catch (err) {
+    res.status(500).json({ message: "There was an error in updating the account." });
+  }
 });
 
 router.delete("/:id", (req, res, next) => {
